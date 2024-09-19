9f495327f715e7ffb9c6b03b7c595eceb3c00fc6
yokozuna753 <brian894526@gmail.com>
2024-09-18 21:49:41 -0400

    worked up to problem 16, will continue

--- problems/16-reverb.js
+++ problems/16-reverb.js
@@ -7,11 +7,44 @@ not a string, say someone passes in a number as an argument, then return null.
 Vowels are the letters "a", "e", "i", "o", "u".
 */
 
-// Your code here 
+/*
+- iterate through the word
+- get the index of the last vowel
+- including the last vowel, cut until the end of the word
+- concat the original word to the last step
+- return null if the argument passed is not a string
+*/
 
-// console.log(reverb('running')); // runninging
-// console.log(reverb('FAMILY'));  // FAMILYILY
-// console.log(reverb('trash'));   // trashash
+// let includes = vowels.includes(word[i])
+function reverb(word) {
+  for (i = word.length - 1; i >= 0; i--) {
+    if (
+      word[i] === "a" ||
+      word[i] === "e" ||
+      word[i] === "i" ||
+      word[i] === "o" ||
+      word[i] === "u"
+    ) {
+      return word + word.slice(i);
+    } else if (
+      word[i] === "A" ||
+      word[i] === "E" ||
+      word[i] === "I" ||
+      word[i] === "O" ||
+      word[i] === "U"
+    ) {
+      return word + word.slice(i);
+    } else if(){
+
+    }else {
+      return null;
+    }
+  }
+}
+
+console.log(reverb("running")); // runninging
+console.log(reverb("FAMILY")); // FAMILYILY
+console.log(reverb("trash")); // trashash
 // console.log(reverb('DISH'));    // DISHISH
 // console.log(reverb('197393'));  // 197393
 // console.log(reverb(197393));    // null
