a555c05456a796d3c714677bdfe8ef5a705d60bc
yokozuna753 <brian894526@gmail.com>
2024-09-27 15:05:34 -0400

    Number 23

--- problems/23-has-double-letter.js
+++ problems/23-has-double-letter.js
@@ -5,15 +5,26 @@ character consecutively. If the value passed into the function is not a string,
 return null.
 */
 
-// Your code here 
+function hasDoubleLetter(str) {
+  if (typeof str !==  "string") {
+    return null;
+  }
+  for (let i = 0; i < str.length - 1; i++) {
+    let char = str[i];
+    if (str[i] === str[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
 
-// console.log(hasDoubleLetter('deer')); // true
-// console.log(hasDoubleLetter('boot camp')); // true
-// console.log(hasDoubleLetter('toggle')); // true
-// console.log(hasDoubleLetter('taco')); // false
-// console.log(hasDoubleLetter('jumper')); // false
-// console.log(hasDoubleLetter(18)); // null
-// console.log(hasDoubleLetter(['array'])); // null
+console.log(hasDoubleLetter("deer")); // true
+console.log(hasDoubleLetter("boot camp")); // true
+console.log(hasDoubleLetter("toggle")); // true
+console.log(hasDoubleLetter("taco")); // false
+console.log(hasDoubleLetter("jumper")); // false
+console.log(hasDoubleLetter(18)); // null
+console.log(hasDoubleLetter(["array"])); // null
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = hasDoubleLetter;
